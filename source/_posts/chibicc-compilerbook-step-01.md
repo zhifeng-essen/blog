397160043a9a1cc351273d2af212a14290f0d867
zhifeng-essen <zhifeng_essen@icloud.com>
2022-08-01 16:42:27 +0800

    chore(font): add font 'LXGW WenKai'

---
+++ source/_posts/chibicc-compilerbook-step-01.md
@@ -0,0 +1,10 @@
+---
+title: 自制 C 语言 - 01 - 编译一个整数
+date: 2022-08-01 14:34:47
+tags: 
+- chibicc
+- compiler
+categories: 
+- 自制 C 编译器
+toc: true
+---
--- themes/icarus/layout/common/article.jsx
+++ themes/icarus/layout/common/article.jsx
@@ -50,9 +50,9 @@ module.exports = class extends Component {
                                 __html: _p('article.created_at', `<time dateTime="${date_xml(page.date)}" title="${new Date(page.date).toLocaleString()}">${date(page.date)}</time>`)
                             }}></span>}
                             {/* Last Update Date */}
-                            {shouldShowUpdated && <span class="level-item" dangerouslySetInnerHTML={{
+                            {/* {shouldShowUpdated && <span class="level-item" dangerouslySetInnerHTML={{
                                 __html: _p('article.updated_at', `<time dateTime="${date_xml(page.updated)}" title="${new Date(page.updated).toLocaleString()}">${date(page.updated)}</time>`)
-                            }}></span>}
+                            }}></span>} */}
                             {/* author */}
                             {page.author ? <span class="level-item"> {page.author} </span> : null}
                             {/* Categories */}
